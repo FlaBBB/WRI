1a0d78c1e9ba268d831fa7d017de214f36111acb
FlaBBB <f12345ff67@gmail.com>
2023-08-30 20:39:07 +0700

    first commit

---
+++ Beta-Test/day-1/one.java
@@ -0,0 +1,9 @@
+public class one {
+    public static void main(String[] args) {
+        int a = 1;
+        String b = "  = one";
+
+        String c = a + b;
+        System.out.println(c);
+    }
+}
